8df0ceb21851b60a56069c353f0d7634d8788045
daviderose526 <daviderose526@gmail.com>
2018-09-14 13:54:14 -0500

    Filled out controller with REST functionality

--- src/main/java/ca/pledgetovote/controllers/PledgeController.java
+++ src/main/java/ca/pledgetovote/controllers/PledgeController.java
@@ -1,13 +1,69 @@
 package ca.pledgetovote.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ca.pledgetovote.model.Pledge;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class PledgeController {
 
+    private List<Pledge> pledges = new ArrayList<>();
+    private AtomicLong nextId = new AtomicLong();
+
     @GetMapping("/hello")
-    public String getHelloMessage () {
+    public String getHelloMessage() {
         return "Hello World!";
     }
+
+    @PostMapping("/pledges")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Pledge createNewPledge(@RequestBody Pledge pledge) {
+        // set pledge to have next ID
+        pledge.setId(nextId.incrementAndGet());
+
+        pledges.add(pledge);
+        return pledge;
+    }
+
+    @GetMapping("/pledges")
+    public List<Pledge> getAllPledges() {
+        return pledges;
+    }
+
+    @GetMapping("/pledges/{id}")
+    public Pledge getOnePledge(@PathVariable("id") long pledgeId) {
+        for(Pledge pledge : pledges) {
+            if(pledge.getId() == pledgeId) {
+                return pledge;
+            }
+        }
+
+        throw new IllegalArgumentException();
+    }
+
+    @PostMapping("pledges/{id}")
+    public Pledge editOnePledge(@PathVariable("id") long pledgeId, @RequestBody Pledge newPledge) {
+        for(Pledge pledge : pledges) {
+            if(pledge.getId() == pledgeId) {
+                pledges.remove(pledge);
+                newPledge.setId(pledgeId);
+                pledges.add(newPledge);
+                return pledge;
+            }
+        }
+
+        throw new IllegalArgumentException();
+    }
+
+    // Create Exception Handler
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "request ID not found.")
+    @ExceptionHandler(IllegalArgumentException.class)
+    public void badIdExceptionHandler() {
+
+    }
+
 }
